7bd9af39d2a41f71191f9353f86888e23bd92efd
Jack LaRue <jacklarue7496@gmail.com>
2018-03-09 18:24:26 -0500

    frontend: Initial setup for button to backend communication. Connect HTML buttons to python actions. Setup for dropdown button. Convert my comments to proper docstring format. (#6)
    * Implement owner buttons
    
    Signed-off-by: Jack LaRue <jvl13@case.edu>
    
    * Add change and conflict functionality
    
    Signed-off-by: Jack LaRue <jvl13@case.edu>
    
    * Setup interior button functionality
    
    Signed-off-by: Jack LaRue <jvl13@case.edu>
    
    * Add personal readme and test
    
    Signed-off-by: Jack LaRue <jvl13@case.edu>
    
    * Fix merge
    
    * Fix changes
    
    * Merge from drop_contents
    
    * Merge from drop_contents branch
    
    Signed-off-by: Jack LaRue <jvl13@case.edu>
    
    * Trim my README
    
    Signed-off-by: Jack LaRue <jvl13@case.edu>
    
    * Reimplement message flashing & initial user functionality logic
    
    * Match style of buttons
    
    * Add backend communication structure
    
    Signed-off-by: Jack LaRue <jvl13@case.edu>
    
    * Link buttons to python functions
    
    Signed-off-by: Jack LaRue <jvl13@case.edu>
    
    * Setup dropdown button
    
    Signed-off-by: Jack LaRue <jvl13@case.edu>
    
    * Convert to docstring format
    
    Signed-off-by: Jack LaRue <jvl13@case.edu>
    
    Original commit: frontend 529175857e0e0f02ec18778a7164d7393436a4c2

--- frontend/syncr_frontend/frontend.py
+++ frontend/syncr_frontend/frontend.py
@@ -19,18 +19,25 @@ app.config.from_envvar('SYNCR_SETTINGS', silent=True)
 
 # Backend Access Functions
 
+# Global Variables
+curr_action = ''
+
 
 def send_message(message):
-    # Sends given message to backend
-    # Wait for a response or until TIMEOUT
-    # If error_message is not None, display error_message
-    # Else display success_message
+    """
+    Sends given message to backend. Waits for a response
+    or until TIMEOUT
+    :param message: message sent to backend
+    :return: If error message is returned
+    display error message. Else display successful message.
+    """
 
     # Example response for initial UI setup
     # TODO: remove when socket communication is setup
     response = {
         'drop_id': message.get('drop_id'),
         'file_name': message.get('file_name'),
+        'file_path': message.get('file_path'),
         'action': message.get('action'),
         'message': "Generic Message For " + message.get('action'),
         'success': True,
@@ -67,6 +74,11 @@ def send_message(message):
 
 
 def open_file_location(file_path):
+    """
+    Opens the location of the file
+    :param file_path: Path of the file on computer
+    :return: opens location of file
+    """
     # Placeholder until backend communication is set-up
     file_path = path.dirname(path.abspath(__file__))
 
@@ -81,7 +93,14 @@ def open_file_location(file_path):
 
 @app.route('/remove_file/<drop_id>/<file_name>')
 def remove_file(drop_id, file_name):
-    # Remove file at specified location from drop info
+    """
+    Removes file at specified location from drop info
+    :param drop_id: id of drop where file is located
+    :param file_name: name of removed file
+    :return: backend and ui removes file instance
+    """
+    set_curr_action('remove file')
+
     message = {
         'drop_id': drop_id,
         'file_name': file_name,
@@ -95,8 +114,10 @@ def remove_file(drop_id, file_name):
     )
 
 
-# Return list of owned drop dictionaries
 def get_owned_drops():
+    """
+    :return: Gets a list of own drop dictionaries
+    """
     message = {
         'action': 'get_owned',
     }
@@ -106,8 +127,10 @@ def get_owned_drops():
     return response.get('requested_drops')
 
 
-# Return list of subscribed drop dictionaries
 def get_subscribed_drops():
+    """
+    :return: Gets a list of subscribed drop dictionaries
+    """
     message = {
         'action': 'get_owned',
     }
@@ -157,6 +180,10 @@ def get_subscribed_drops():
 
 # Return dictionary for selected drop
 def get_selected_drop(drop_id):
+    """
+    :param drop_id: Selected drop
+    :return: Dictionary for selected drop
+    """
     message = {
         'drop_id': drop_id,
         'action': 'get_selected',
@@ -172,13 +199,35 @@ def get_selected_drop(drop_id):
         return drop_list[0]
 
 
+@app.route('/get_conflicting_files/<drop_id>')
 def get_conflicting_files(drop_id):
-    # eventually will be used to retrieve files of conflicting drops
-    pass
+    """
+    Retrieves conflicting files from a drop.
+    :param drop_id: ID of drop with conflicting files.
+    :return: list of conflicting files
+    """
+    set_curr_action('get conflicting files')
+
+    message = {
+        'drop_id': drop_id,
+        'action':  'get_c_f',
+    }
+
+    # TODO: Retrieve conflicting files names from backend.
+    response = send_message(message)
+
+    return show_drops(
+        response.get('drop_id'),
+        response.get('message') + " of drop " + response.get('drop_id'),
+    )
 
 
 def get_permission(drop_id):
-    # returns the permission type of the drop ID
+    """
+    Returns the permission type of the drop ID.
+    :param drop_id: The ID of a given drop.
+    :return: The permission type of the drop.
+    """
     owned_drops = get_owned_drops()
 
     for drop in owned_drops:
@@ -188,44 +237,212 @@ def get_permission(drop_id):
     return "subscribed"
 
 
-def decline_conflict_file(file_path):
-    # if a file is in conflict with master
-    # declining changes leaves file on master the same
-    # backend communication: remove conflict file
-    return
+def get_drop_id(file_path):
+    """
+    Gets drop id from file path
+    :param file_path: File path that contains drop ID
+    :return: drop ID
+    """
+
+    reached_slash = False
+    drop_string = ''
+
+    for letter in file_path:
+        if not reached_slash:
+            if letter == '/':
+                reached_slash = True
+            else:
+                drop_string = drop_string + letter
+
+    return drop_string
 
 
-def accept_conflict_file(file_path):
-    # if a file is in conflict with file in master
-    # accepting changes modifies master file
-    # backend communication: change master file
-    return
+def get_file_name(file_path):
+    """
+    Gets file name from file path
+    :param file_path: File path that contains file name
+    :return: file name
+    """
+
+    file_name = ''
+
+    for letter in file_path:
+        if letter == '/':
+            file_name = ''
+        else:
+            file_name = file_name + letter
+
+    return file_name
 
 
-def accept_changes(file_path):
-    # Accepts the proposed changes of a file
-    # backend: modify the master file with proposed changes
-    return
+def set_curr_action(action_update):
+    """
+    Sets the global variable to the current pressed
+    button
+    :param action_update: name of action
+    :return: none
+    """
+
+    global curr_action
+    curr_action = action_update
 
 
-def decline_changes(file_path):
-    # Declines the proposed changes of a file
-    # backend: discard changes, keep master file
-    return
+def get_file_versions(file_path):
+    """
+    Retrieves available versions of a file
+    :param file_path: Path of conflicted file
+    :return: list of versions for particular file
+    """
+    # TODO: link with backend to retrieve file version info.
+
+    return {
+        'versions': [
+            {'name': 'Version 1', 'timestamp': 'ts1', 'owner': 'o1'},
+            {'name': 'Version 2', 'timestamp': 'ts2', 'owner': 'o2'},
+            {'name': 'Version 3', 'timestamp': 'ts3', 'owner': 'o3'},
+            {'name': 'Version 4', 'timestamp': 'ts4', 'owner': 'o4'},
+        ],
+    }
+
+
+# TODO: Get file name from path so that file_name isn't parameter.
+@app.route('/decline_conflict_file/<file_path>/<file_name>')
+def decline_conflict_file(file_path, file_name):
+    """
+    Sends 'decline conflict file' command to backend
+    :param file_path: path of the declined file
+    :param file_name: name of the declined file
+    :return: message sent back to frontend
+    """
+
+    message = {
+        'drop_id': get_drop_id(file_path),
+        'file_path': file_path,
+        'file_name': file_name,
+        'action': 'd_c_f',
+    }
+
+    # TODO: Communicate to do nothing to the master file on backend
+    response = send_message(message)
+
+    return show_drops(
+        response.get('drop_id'),
+        response.get('message') + " of file " + response.get('file_name'),
+    )
+
+
+# TODO: Get file name from path so that file_name isn't parameter.
+@app.route('/accept_conflict_file/<file_path>/<file_name>')
+def accept_conflict_file(file_path, file_name):
+    """
+    Sends 'accept conflict file' command to backend
+    :param file_path: path of the accepted file
+    :param file_name: name of the accepted file
+    :return: message sent back to frontend
+    """
+
+    message = {
+        'drop_id': get_drop_id(file_path),
+        'file_path': file_path,
+        'file_name': file_name,
+        'action': 'a_c_f',
+    }
+
+    # TODO: Backend will modify master file with changes
+    response = send_message(message)
+
+    return show_drops(
+        response.get('drop_id'),
+        response.get('message') + " of file " + response.get('file_name'),
+    )
+
+
+# TODO: Get file name from path so that file_name isn't parameter.
+@app.route('/accept_changes/<file_path>/<file_name>')
+def accept_changes(file_path, file_name):
+    """
+    Sends 'accept changes' command to backend
+    :param file_path: path of file with accepted changes
+    :param file_name: name of file with accepted changes
+    :return: message sent back to frontend
+    """
+
+    message = {
+        'drop_id': get_drop_id(file_path),
+        'file_path': file_path,
+        'file_name': file_name,
+        'action': 'a_c',
+    }
+
+    # TODO: Backend will modify master file with changes
+    response = send_message(message)
+
+    return show_drops(
+        response.get('drop_id'),
+        response.get('message') + " of file " + response.get('file_name'),
+    )
+
+
+# TODO: Get file name from path so that file_name isn't parameter.
+@app.route('/decline_changes/<file_path>/<file_name>')
+def decline_changes(file_path, file_name):
+    """
+    Sends 'decline changes' command to backend
+    :param file_path: path of file with declined changes
+    :param file_name: name of file with declined changes
+    :return: message sent back to frontend
+    """
+
+    message = {
+        'drop_id': get_drop_id(file_path),
+        'file_path': file_path,
+        'file_name': file_name,
+        'action': 'd_c',
+    }
+
+    # TODO: Backend will keep master file unchanged
+    response = send_message(message)
+
+    return show_drops(
+        response.get('drop_id'),
+        response.get('message') + " of file " + response.get('file_name'),
+    )
 
 
 @app.route('/view_conflicts/<drop_id>')
 def view_conflicts(drop_id):
-    # if no drop is selected
-        # do nothing
-    # else
-        # retrieve files from all conflicting drops
-        # display said files in body of page
-    return show_drops(drop_id, "Current conflicts")
+    """
+    Sends 'view conflicts' command to backend
+    :param drop_id: name of drop with possible conflicts
+    :return: list of conflicted files sent to frontend
+    """
+
+    set_curr_action('current conflicts')
+
+    message = {
+        'drop_id': drop_id,
+        'action':  'v_c',
+    }
+
+    # TODO: Setup communication to retrieve conflicting files.
+    response = send_message(message)
+
+    # TODO: Get global variable setup for selected button (HTML)
+    return show_drops(
+        response.get('drop_id'),
+        response.get('message') + " of drop " + response.get('drop_id'),
+    )
 
 
 @app.route('/add_file/<drop_id>')
 def add_file(drop_id):
+    """
+    Sends 'add file' message to backend
+    :param drop_id: ID of drop where file is added
+    :return: Prompt to add a file
+    """
+
+    set_curr_action('add file')
 
     file_path = None  # TODO: implement file picker (tkinter not working)
 
@@ -246,6 +463,14 @@ def add_file(drop_id):
 
 @app.route('/share_drop/<drop_id>')
 def share_drop(drop_id):
+    """
+    Sends 'share drop' message to backend
+    :param drop_id: Drop to be shared
+    :return: backend sends back message with info to share drop
+    """
+
+    set_curr_action('share drop')
+
     message = {
         'drop_id': drop_id,
         'action': 'share',
@@ -258,44 +483,85 @@ def share_drop(drop_id):
 
 @app.route('/view_pending_changes/<drop_id>')
 def view_pending_changes(drop_id):
-    # if no drop is selected
-        # do nothing
-    # else
-        # display pending file changes on body of page
-        # should provide options to review/accept pending changes
-    return show_drops(drop_id, "view pending changes")
+    """
+    Sends 'view pending changes' command to backend
+    :param drop_id: name of drop with pending changes
+    :return: list of files in drop with proposed changes
+    """
+
+    set_curr_action('view pending changes')
+
+    message = {
+        'drop_id': drop_id,
+        'action': 'v_p_c',
+    }
+
+    # TODO: Setup communication to retrieve files with changes.
+    response = send_message(message)
+
+    return show_drops(
+        response.get('drop_id'),
+        response.get('message') + " of drop " + response.get('drop_id'),
+    )
 
 
 @app.route('/view_owners/<drop_id>')
 def view_owners(drop_id):
-    # communicate with backend to retrieve owners
-    # display owners on body of page
-    # give user option to remove owners if primary owner
+    """
+    Communicate with backend to retrieve owners
+    :param drop_id: ID of drop to view owners
+    :return: display owners on body of page
+    gives user option to remove owner if primary owner
+    """
+    set_curr_action('view owners')
     return show_drops(drop_id, "list of owners")
 
 
 @app.route('/whitelist/<drop_id>')
 def whitelist(drop_id):
-    # if no drop is selected
-        # do nothing
-    # else
-        # display prompt on page to whitelist node
-        # prompt should communicate with backend
+    """
+    Communicate with backend to whitelist node
+    :param drop_id: ID of drop where node is whitelisted
+    :return: Display prompt on page to whitelist node
+    """
+    set_curr_action('whitelist')
+
     return show_drops(drop_id, "node whitelisted")
 
 
 @app.route('/delete_drop/<drop_id>')
 def delete_drop(drop_id):
-    # if no drop is selected
-        # do nothing
-    # else
-        # communicate deletion to backend.
-        # backend should delete drop?
-    return show_drops(drop_id, "drop deleted")
+    """
+    Sends the 'delete drop' message to backend
+    :param drop_id: name of the drop to be deleted
+    :return: drop is removed from backend and frontend
+    """
+
+    set_curr_action('delete drop')
+
+    message = {
+        'drop_id': drop_id,
+        'action': 'd_drop',
+    }
+
+    # TODO: Setup backend communication to remove drop
+    response = send_message(message)
+
+    return show_drops(
+        response.get('drop_id'),
+        response.get('message') + " of drop " + response.get('drop_id'),
+    )
 
 
 @app.route('/unsubscribe/<drop_id>')
 def unsubscribe(drop_id):
+    """
+    Communicate with backend to unsubscribe from drop
+    :param drop_id: ID of drop to be unsubscribed
+    :return: UI and backend update to not have subscribed drop.
+    """
+
+    set_curr_action('unsubscribe')
     message = {
         'drop_id': drop_id,
         'action': 'unsub',
@@ -314,6 +580,14 @@ def unsubscribe(drop_id):
 # Request a change to the selected drop
 @app.route('/request_change/<drop_id>')
 def request_change(drop_id):
+    """
+    Communicate with backend to request change
+    :param drop_id: ID of drop with changes
+    :return: UI and backend update with proposed change
+    """
+
+    set_curr_action('request change')
+
     message = {
         'drop_id': drop_id,
         'action': 'change',
@@ -330,9 +604,17 @@ def startup():
 
 @app.route('/<drop_id>', methods=['GET', 'POST'])
 def show_drops(drop_id=None, message=None):
+    """
+    Main action handler. Shows drops
+    :param drop_id: ID of current drop
+    :param message: Message from a particular action
+    :return: renders web page based off of drop and action.
+    """
     owned_drops = get_owned_drops()
     subscribed_drops = get_subscribed_drops()
     selected_drop = []
+
+    file_versions = get_file_versions('')  # REMOVE WHEN BACKEND IS ADDED
 
     if drop_id is not None:
         selected_drop = get_selected_drop(drop_id)
@@ -353,5 +635,6 @@ def show_drops(drop_id=None, message=None):
 
     return render_template(
         'show_drops.html', selected=selected_drop, subscribed=subscribed_drops,
-        owned=owned_drops, action=performed_action,
+        owned=owned_drops, action=performed_action, selec_act=curr_action,
+        versions=file_versions,
     )
